3cc8fcee1de9d8500b2e2ba251a90dc5e548afa8
MarlonPrado <62295874+MarlonPrado@users.noreply.github.com>
2022-05-09 05:42:13 +0200

    Update mostrarUno.js
    Añadiendo el FindOne()

--- src/controller/evento/mostrarUno.js
+++ src/controller/evento/mostrarUno.js
@@ -1,1 +1,9 @@
-export default null;
+import evento from "@Model/evento"
+
+export default async (req, res) => {
+    const eventos = await evento.findOne()
+    console.log('Si compila aqui, ta bien');
+    res.status(200).json(eventos)
+}
+
+//Tomado de https://sequelize.org/docs/v6/core-concepts/model-querying-finders/
