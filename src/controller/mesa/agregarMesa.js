80e1e2347566f34007ffebffc1fb2967f551dad5
Jorge Hernandez <jorgekevinhl@ufps.edu.co>
2022-05-25 11:42:51 -0500

    Correcion en crear mesa

--- src/controller/mesa/agregarMesa.js
+++ src/controller/mesa/agregarMesa.js
@@ -2,11 +2,11 @@ import mesa from '@Model/mesa';
 
 export default async (req, res) => {
   const {
-    nombre, descripcion, correo, id_uni
+    nombre, descripcion, correo, universidad_id
   } = req.body
 
   try{
-    const noti = await mesa.create({nombre, descripcion, correo, id_uni});
+    const noti = await mesa.create({nombre, descripcion, correo, universidad_id});
     res.status(200).send({message: `Mesa creada con exito`})
   }catch(e){
     res.status(500).send({message: `Error al crear la mesa: ${e}`})
