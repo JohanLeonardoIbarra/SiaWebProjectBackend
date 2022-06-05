cd94adb37d8b6045f0ab3fc22148b9027ddcc777
@johanleonardois <johanleonardois@ufps.edu.co>
2022-06-04 22:27:32 -0500

    rebuild

--- build/routes/mesa.js
+++ build/routes/mesa.js
@@ -13,6 +13,6 @@ const router = new _express.Router();
 router.get('/mesas', _mesa.mostrarTodos);
 router.post('/mesas', _mesa.agregarMesa);
 router.put('/mesas/:id', _mesa.editarMesa);
-router.delete('/mesas/:id', _mesa.editarMesa);
+router.delete('/mesas/:id', _mesa.eliminarMesa);
 var _default = router;
 exports.default = _default;
